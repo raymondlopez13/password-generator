7dae09ae630607c496d70c4af0b06cc8be5c3ab1
Raymond Lopez <raymondlopez13@yahoo.com>
2020-12-17 19:17:51 -0600

    Get information from user as well as validate that information

--- Develop/script.js
+++ Develop/script.js
@@ -1,5 +1,40 @@
 // Assignment code here
-
+function generatePassword() {
+  var passwordLength = prompt("Length of password (8-128 characters)");
+  if (passwordLength < 8){
+    alert('Please choose a number between 8 and 128 characters');
+    generatePassword();
+  } else if (passwordLength > 128) {
+    alert('Please choose a number between 8 and 128 characters');
+    generatePassword();
+  }
+  var lowercase = confirm("Would you like lowercase letters in your password?");
+  lowercase = confirm("Are you sure?");
+  var uppercase = confirm("Uppercase letters?");
+  uppercase = confirm("Are you sure?");
+  var numbers = confirm("Numbers?");
+  numbers = confirm("Are you sure?");
+  var specialChar = confirm("Special Characters?");
+  specialChar = confirm("Are you sure?");
+  if (lowercase === false && uppercase === false && numbers === false && specialChar === false){
+    alert('I cannot create a password made of nothing!');
+    generatePassword();
+  }
+  var arr = [];
+  if (lowercase === true) {
+    arr.push('lower');
+  } 
+  if (uppercase === true) {
+    arr.push('upper');
+  } 
+  if (numbers === true) {
+    arr.push('number');
+  } 
+  if (specialChar === true) {
+    arr.push('special character')
+  }
+  console.log(arr);
+}
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
